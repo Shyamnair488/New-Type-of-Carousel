e5f14bba091396fd3e77a8f600facd74fdd07693
Shyam Nair <110102576+Shyamnair488@users.noreply.github.com>
2025-03-11 22:38:02 +0530

    Your new commit message

--- components/Header.tsx
+++ components/Header.tsx
@@ -12,25 +12,25 @@ const Header = () => {
             <div className="flex space-x-8 ml-2">
               <Link 
                 href="/" 
-                className="text-gray-700 hover:text-black text-2xl font-semibold tracking-wide transition-all duration-300 hover:scale-105 no-underline"
+                className="text-gray-900 hover:text-black text-2xl font-semibold tracking-wide transition-all duration-300 hover:scale-105 no-underline"
               >
                 Home
               </Link>
               <Link 
                 href="/about" 
-                className="text-gray-700 hover:text-black text-2xl font-semibold tracking-wide transition-all duration-300 hover:scale-105 no-underline"
+                className="text-gray-900 hover:text-black text-2xl font-semibold tracking-wide transition-all duration-300 hover:scale-105 no-underline"
               >
                 About
               </Link>
               <Link 
                 href="/services" 
-                className="text-gray-700 hover:text-black text-2xl font-semibold tracking-wide transition-all duration-300 hover:scale-105 no-underline"
+                className="text-gray-900 hover:text-black text-2xl font-semibold tracking-wide transition-all duration-300 hover:scale-105 no-underline"
               >
                 Services
               </Link>
               <Link 
                 href="/contact" 
-                className="text-gray-700 hover:text-black text-2xl font-semibold tracking-wide transition-all duration-300 hover:scale-105 no-underline"
+                className="text-gray-900 hover:text-black text-2xl font-semibold tracking-wide transition-all duration-300 hover:scale-105 no-underline"
               >
                 Contact
               </Link>
@@ -40,7 +40,7 @@ const Header = () => {
             <div className="flex space-x-8 mr-8">
               <Link 
                 href="/user-login" 
-                className="text-gray-700 hover:text-white text-xl font-semibold tracking-wide no-underline transition-all duration-300 hover:scale-105 bg-white hover:bg-gray-800 rounded-lg px-6 py-2 border-2 border-gray-700 hover:border-gray-800 shadow-md"
+                className="text-gray-900 hover:text-white text-xl font-semibold tracking-wide no-underline transition-all duration-300 hover:scale-105 bg-white hover:bg-gray-800 rounded-lg px-6 py-2 border-2 border-gray-700 hover:border-gray-800 shadow-md"
                 style={{ textDecoration: 'none' }}
               >
                 User Login
